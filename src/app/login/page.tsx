ee53681db55ef6ac769cfa6d80ce43712d27f24b
vedant2863 <jvedant727@gmail.com>
2024-01-22 22:19:50 +0530

    created signup and login ui

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,46 @@
+"use client";
+import Link from "next/link";
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+
 export default function LoginPage() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+  });
+
+  const onLogin = async () => {};
+
   return (
-    <div className="flex" >
-      <h1 className="text-center text-white text-2xl" >Login</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1> Login</h1>
+      <hr />
+      <label htmlFor="email">email</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+        id="email"
+        type="text"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="email"
+      />
+      <label htmlFor="password">password</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+        id="password"
+        type="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="password"
+      />
+      <button
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        onClick={onLogin}
+      >
+        Login here
+      </button>
+      <Link href="/signup">Visit Signup page</Link>
     </div>
   );
 }
