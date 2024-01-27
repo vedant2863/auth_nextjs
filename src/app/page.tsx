1d5e6c8dc5603edaeb56535009c19eb4774e79ca
vedant2863 <jvedant727@gmail.com>
2024-01-27 18:54:04 +0530

    debug code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -83,7 +83,7 @@ export default function Home() {
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Templates{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+              
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,12 +1,14 @@
-export default function UserProfile({params}: any) {
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1>Profile</h1>
-            <hr />
-            <p className="text-4xl">Profile page 
-            <span className=" p-2 ml-2 rounded bg-orange-500 text-black">{params.id}</span>
-            </p>
-
-            </div>
-    )
+export default function UserProfile({ params }: any) {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="mb-10 text-4xl text-purple-900 hover:text-orange-500 " >Profile</h1>
+      <hr />
+      <p className="text-4xl">
+        Profile page
+        <span className=" p-2 ml-2 rounded bg-orange-500 text-black">
+          {params.id}
+        </span>
+      </p>
+    </div>
+  );
 }
